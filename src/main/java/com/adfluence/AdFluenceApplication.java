b3682a7025463e1022829bd62b17ab7403444b14
Omar <59146036+omarmahamid@users.noreply.github.com>
2023-03-17 16:20:07 +0200

    Add files via upload

---
+++ src/main/java/com/adfluence/AdFluenceApplication.java
@@ -0,0 +1,13 @@
+package com.adfluence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdFluenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdFluenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adfluence/AdFluenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adfluence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdFluenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
