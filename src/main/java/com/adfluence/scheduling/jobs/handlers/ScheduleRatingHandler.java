4769992a6cf08449d43b2dd872f378fe7e25b1f2
omar.mahamid <omar.taiseerr@gmail.com>
2023-05-14 16:17:26 +0300

    S-111update

--- src/main/java/com/adfluence/scheduling/jobs/handlers/ScheduleRatingHandler.java
+++ src/main/java/com/adfluence/scheduling/jobs/handlers/ScheduleRatingHandler.java
@@ -4,14 +4,26 @@ import com.adfluence.scheduling.Job;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Date;
+
 public class ScheduleRatingHandler implements IScheduleHandler{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleRatingHandler.class);
-
-
     @Override
     public void handle(Job job) {
 
-        LOGGER.info("Hello from Job {}", Thread.currentThread().getName());
+        Date date = new Date();
+
+        if (date.before(job.getResource().getDate())){
+            process();
+        }else {
+            LOGGER.info("Hello from Job {}", Thread.currentThread().getName());
+        }
     }
+
+
+    private void process(){
+
+    }
+
 }
